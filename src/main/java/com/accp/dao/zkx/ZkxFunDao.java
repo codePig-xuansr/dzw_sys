abc31f02060123c1ddc8fbe2242ea229c8c87fb7
xuansr <842704182@qq.com>
2020-03-31 13:24:36 +0800

    zkx提交权限查询

--- src/main/java/com/accp/dao/zkx/ZkxFunDao.java
+++ src/main/java/com/accp/dao/zkx/ZkxFunDao.java
@@ -1,8 +1,13 @@
 package com.accp.dao.zkx;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.accp.pojo.zkx.ZkxFunction;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 public interface ZkxFunDao extends BaseMapper<ZkxFunction>{
 
+	public List<ZkxFunction> showFunByUser(@Param("user")String user,@Param("pwd")String pwd);
 }
--- src/main/java/com/accp/pojo/zkx/ZkxFunction.java
+++ src/main/java/com/accp/pojo/zkx/ZkxFunction.java
@@ -1,6 +1,12 @@
 package com.accp.pojo.zkx;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+@TableName("function")
 public class ZkxFunction {
+	@TableId(type = IdType.AUTO,value = "fid")
     private Integer fid;
 
     private String fcode;
@@ -13,7 +19,12 @@ public class ZkxFunction {
         return fid;
     }
 
-    public void setFid(Integer fid) {
+    @Override
+	public String toString() {
+		return "ZkxFunction [fid=" + fid + ", fcode=" + fcode + ", fname=" + fname + ", fpid=" + fpid + "]";
+	}
+
+	public void setFid(Integer fid) {
         this.fid = fid;
     }
 
