c349c2f06e82b9c1ace6040b0b3098eb1b73aad3
IPQYYGY <1529260641@qq.com>
2020-04-03 21:45:41 +0800

    sw4月3日提交后端

--- src/main/java/com/accp/action/sw/WeixiuAction.java
+++ src/main/java/com/accp/action/sw/WeixiuAction.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.accp.biz.sw.WeixiuBiz;
 import com.accp.vo.sw.WeixiuVo;
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageInfo;
 
 @RestController
@@ -15,6 +16,31 @@ import com.github.pagehelper.PageInfo;
 public class WeixiuAction {
 	@Autowired
 	private WeixiuBiz biz;
+
+	/**
+	 * 根据维修单号查询
+	 * 
+	 * @param recordid
+	 * @return
+	 */
+	@GetMapping("/{recordid}")
+	public PageInfo<WeixiuVo> queyPageByRecordid(@PathVariable String recordid) {
+		return biz.findPageByRecordid(recordid);
+	}
+
+	/**
+	 * 根据结算状态查询维修分页并查询
+	 * 
+	 * @param pageNum
+	 * @param pageSize
+	 * @param carno
+	 * @return
+	 */
+	@GetMapping("/status/{pageNum}/{pageSize}/{status}")
+	public PageInfo<WeixiuVo> queryPageCarno(@PathVariable Integer pageNum, @PathVariable Integer pageSize,
+			@PathVariable Integer status) {
+		return biz.findPageByStatus(pageNum, pageSize, status);
+	}
 
 	/**
 	 * 根据客户id查询消费分页并查询
--- src/main/java/com/accp/dao/sw/IWeixiuDao.java
+++ src/main/java/com/accp/dao/sw/IWeixiuDao.java
@@ -8,5 +8,20 @@ import com.accp.vo.sw.WeixiuVo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 public interface IWeixiuDao {
-	public List<WeixiuVo> queryListById(@Param("id") Integer id,@Param("carno") String carno);
+	/**
+	 * 根据客户id或车牌查询消费记录
+	 * 
+	 * @param id
+	 * @param carno
+	 * @return
+	 */
+	public List<WeixiuVo> queryListById(@Param("id") Integer id, @Param("carno") String carno);
+
+	/**
+	 * 根据状态查询维修
+	 * 
+	 * @param status
+	 * @return
+	 */
+	public List<WeixiuVo> queryListByJsstatus(@Param("status") Integer status,@Param("recordid") String recordid);
 }
